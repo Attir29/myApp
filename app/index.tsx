a5e3a0955cb77d49be4c10bf5217d7064e36ae7d
Attir29 <attirmidziislamipratama@gmail.com>
2025-10-21 10:43:22 +0700

    coba tag text & textinput

--- app/index.tsx
+++ app/index.tsx
@@ -1,4 +1,4 @@
-import { Text, View } from "react-native";
+import { Text, TextInput, View } from "react-native";
 
 export default function Index() {
   return (
@@ -9,7 +9,10 @@ export default function Index() {
         alignItems: "center",
       }}
     >
-      <Text>Edit app/index.tsx to edit this screen.</Text>
+      <Text>aplikasi data diri saya</Text>
+      <TextInput placeholder="nama lengkap" style={{ borderWidth: 1, width: "80%", marginVertical: 10, borderRadius: 12, padding: 18 }} />
+      <TextInput placeholder="email" style={{ borderWidth: 1, width: "80%", marginVertical: 10, borderRadius: 12, padding: 18 }} />
+      <TextInput placeholder="alamat" style={{ borderWidth: 1, width: "80%", marginVertical: 10, borderRadius: 12, padding: 18 }} />
     </View>
   );
 }
